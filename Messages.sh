3ba6bf43bfe4ed0a5d8437e17b1e3a6be2778689
Harald Sitter <sitter@kde.org>
2011-01-28 16:00:08 +0100

    ok, so we already had a Messages.sh, though it was in gstreamer/, move it to the main dir to prevent me from adding another Messages.sh in a year or two ;)

--- Messages.sh
+++ Messages.sh
@@ -1,2 +1,5 @@
-#!/usr/bin/env bash
-$XGETTEXT_QT `find gstreamer/ -name '*.cpp'` -o $podir/phonon-backend-gstreamer.pot
+#! /usr/bin/env bash
+find gstreamer/ -maxdepth 1 -name "*.cpp" -print > files
+find gstreamer/ -maxdepth 1 -name "*.h" -print >> files
+$XGETTEXT_QT --copyright-holder=This_file_is_part_of_KDE --msgid-bugs-address=http://bugs.kde.org --files-from=files -o $podir/phonon_gstreamer.pot
+rm files
