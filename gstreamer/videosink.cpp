4f3b182cea01328ab887b8a59c791d33b2a3bdb3
Harald Sitter <sitter@kde.org>
2011-06-21 20:30:55 +0200

    add videosink stub

---
+++ gstreamer/videosink.cpp
@@ -0,0 +1,11 @@
+#include "videosink.h"
+
+namespace Phonon {
+namespace Gstreamer {
+
+VideoSink::VideoSink()
+{
+}
+
+} // namespace Gstreamer
+} // namespace Phonon
---
+++ gstreamer/videosink.h
@@ -0,0 +1,16 @@
+#ifndef PHONON_GSTREAMER_VIDEOSINK_H
+#define PHONON_GSTREAMER_VIDEOSINK_H
+
+namespace Phonon {
+namespace Gstreamer {
+
+class VideoSink
+{
+public:
+    VideoSink();
+};
+
+} // namespace Gstreamer
+} // namespace Phonon
+
+#endif // PHONON_GSTREAMER_VIDEOSINK_H
