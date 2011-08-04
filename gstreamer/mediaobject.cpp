a6f31769068ca28761404bb69359822ebb2ee340
Trever Fischer <tdfischer@fedoraproject.org>
2011-08-04 16:00:29 -0400

    /really/ fix deadlock :x

--- gstreamer/mediaobject.cpp
+++ gstreamer/mediaobject.cpp
@@ -251,7 +251,7 @@ void MediaObject::setError(const QString &errorString, Phonon::ErrorType error)
     m_error = error;
     // Perform this asynchronously because this is also called from within the pipeline's cb_error
     // handler, which can cause deadlock once we finally get down to gst_element_set_state
-    QMetaObject::invokeMethod(this, "requestState", Q_ARG(Phonon::State, Phonon::ErrorState));
+    QMetaObject::invokeMethod(this, "requestState", Qt::QueuedConnection, Q_ARG(Phonon::State, Phonon::ErrorState));
 }
 
 qint64 MediaObject::totalTime() const
