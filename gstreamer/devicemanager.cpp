ca66d4217544e3797d9119afba400bd20fe1e674
Casian Andrei <skeletk13@gmail.com>
2012-02-21 17:34:51 +0200

    Put discoverer's icon info for phonon-gstreamer devices

--- gstreamer/devicemanager.cpp
+++ gstreamer/devicemanager.cpp
@@ -399,6 +399,7 @@ QHash<QByteArray, QVariant> DeviceManager::deviceProperties(int id)
             properties.insert("description", device.description());
             properties.insert("isAdvanced", device.isAdvanced());
             properties.insert("deviceAccessList", QVariant::fromValue<Phonon::DeviceAccessList>(device.accessList()));
+            properties.insert("discovererIcon", QLatin1String("phonon-gstreamer"));
 
             if (device.capabilities() & DeviceInfo::AudioOutput) {
                 properties.insert("icon", QLatin1String("audio-card"));
