7816b9e9f7485fcdd92203501c057a8aa261f34b
Rohan Garg <rohangarg@kubuntu.org>
2013-03-16 04:50:24 +0530

    Make sure we fallback to GStreamer 0.10 if GStreamer 1.0 is not found

--- gstreamer/ConfigureChecks.cmake
+++ gstreamer/ConfigureChecks.cmake
@@ -12,10 +12,17 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
-find_package(GStreamer REQUIRED)
-macro_log_feature(GSTREAMER_FOUND "GStreamer" "gstreamer 0.10 is required for the multimedia backend" "http://gstreamer.freedesktop.org/modules/" TRUE "0.10")
+find_package(GStreamer)
 
-find_package(GStreamerPlugins REQUIRED)
+if (GSTREAMER_FOUND)
+    macro_log_feature(GSTREAMER_FOUND "GStreamer" "gstreamer 0.10 is required for the multimedia backend" "http://gstreamer.freedesktop.org/modules/" TRUE "1.0")
+    find_package(GStreamerPlugins REQUIRED)
+else()
+    find_package(GStreamer0.10 REQUIRED)
+    macro_log_feature(GSTREAMER_FOUND "GStreamer" "gstreamer 0.10 is required for the multimedia backend" "http://gstreamer.freedesktop.org/modules/" TRUE "0.10" REQUIRED)
+    find_package(GStreamerPlugins0.10 REQUIRED)
+endif (GSTREAMER_FOUND)
+
 macro_log_feature(GSTREAMER_PLUGIN_VIDEO_FOUND "GStreamer video plugin" "The gstreamer video plugin (part of gstreamer-plugins-base 0.10) is required for the multimedia gstreamer backend" "http://gstreamer.freedesktop.org/modules/" TRUE "0.10")
 macro_log_feature(GSTREAMER_PLUGIN_AUDIO_FOUND "GStreamer audio plugin" "The gstreamer audio plugin (part of gstreamer-plugins-base 0.10) is required for the multimedia gstreamer backend" "http://gstreamer.freedesktop.org/modules/" TRUE "0.10")
 macro_log_feature(GSTREAMER_PLUGIN_PBUTILS_FOUND "GStreamer pbutils plugin" "The gstreamer pbutils plugin (part of gstreamer-plugins-base 0.10) is required for the multimedia gstreamer backend" "http://gstreamer.freedesktop.org/modules/" TRUE "0.10")
