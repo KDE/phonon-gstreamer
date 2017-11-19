e9f47800d88ab665895b20e90b06fa757565ee36
l10n daemon script <scripty@kde.org>
2017-11-19 03:04:42 +0100

    SVN_SILENT made messages (.desktop file)

--- gstreamer/gstreamer.desktop.cmake
+++ gstreamer/gstreamer.desktop.cmake
@@ -13,10 +13,12 @@ Name=GStreamer
 Name[cs]=GStreamer
 Name[fr]=GStreamer
 Name[nl]=GStreamer
+Name[sk]=GStreamer
 Name[x-test]=xxGStreamerxx
 
 Comment=Phonon GStreamer backend
 Comment[cs]=Podpůrná vrstva GStreamer pro Phonon
 Comment[fr]=Moteur GStreamer pour Phonon 
 Comment[nl]=GStreamer-backend (Phonon)
+Comment[sk]=Podsystém Phonon GStreamer
 Comment[x-test]=xxPhonon GStreamer backendxx
