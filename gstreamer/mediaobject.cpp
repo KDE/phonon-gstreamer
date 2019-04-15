KDE/phonon-gstreamer

commit 73128a60dae2c3fb2f2348f4ca42e08aac21589b

Avoid using subtitles of a previously played media
Summary: When a media object detected a subtitle, this one is used in all subsequent playback until another subtitle  is set.

Test Plan: Localy tested.

Reviewers: O4 phonon, dvratil

Reviewed By: dvratil

Differential Revision: https://phabricator.kde.org/D20379

gstreamer/mediaobject.cpp
@@ -301,24 +301,32 @@ MediaSource MediaObject::source() const
 
 void MediaObject::changeSubUri(const Mrl &mrl)
 {
-    QString fontDesc;
-    QByteArray customFont = qgetenv("PHONON_SUBTITLE_FONT");
-    QByteArray customEncoding = qgetenv("PHONON_SUBTITLE_ENCODING");
+    if (mrl.isEmpty()) {
+        g_object_set_property(G_OBJECT(m_pipeline->element()),
+            "suburi", NULL);
+    } else {
+        QString fontDesc;
+        QByteArray customFont = qgetenv("PHONON_SUBTITLE_FONT");
+        QByteArray customEncoding = qgetenv("PHONON_SUBTITLE_ENCODING");
 
-    if (customFont.isNull()) {
-        QFont videoWidgetFont = QApplication::font("VideoWidget");
-        fontDesc = videoWidgetFont.family() + ' ' + QString::number(videoWidgetFont.pointSize());
+        if (customFont.isNull()) {
+            QFont videoWidgetFont = QApplication::font("VideoWidget");
+            fontDesc = videoWidgetFont.family() + ' ' + QString::number(videoWidgetFont.pointSize());
+        }
+        //FIXME: Try to detect common encodings, like libvlc does
+        g_object_set(G_OBJECT(m_pipeline->element()),
+            "suburi", mrl.toEncoded().constData(),
+            "subtitle-font-desc", customFont.isNull() ? fontDesc.toStdString().c_str() : customFont.constData(),
+            "subtitle-encoding", customEncoding.isNull() ? "UTF-8" : customEncoding.constData(),
+            NULL);
     }
-    //FIXME: Try to detect common encodings, like libvlc does
-    g_object_set(G_OBJECT(m_pipeline->element()),
-        "suburi", mrl.toEncoded().constData(),
-        "subtitle-font-desc", customFont.isNull() ? fontDesc.toStdString().c_str() : customFont.constData(),
-        "subtitle-encoding", customEncoding.isNull() ? "UTF-8" : customEncoding.constData(),
-        NULL);
 }
 
 void MediaObject::autoDetectSubtitle()
 {
+    // clean previously associated subtitle
+    changeSubUri(Mrl());
+
     if (m_source.type() == MediaSource::LocalFile ||
        (m_source.type() == MediaSource::Url && m_source.mrl().scheme() == "file") ) {
 

commit 2c5c6494bb6a5bbd15b23858200185affcd346b7

Fix a crash.
Summary: Fix a regression introduced in D20379

Test Plan: Compiles and runs

Reviewers: dvratil, O4 phonon

Reviewed By: dvratil

Differential Revision: https://phabricator.kde.org/D20539

gstreamer/mediaobject.cpp
@@ -302,8 +302,8 @@ MediaSource MediaObject::source() const
 void MediaObject::changeSubUri(const Mrl &mrl)
 {
     if (mrl.isEmpty()) {
-        g_object_set_property(G_OBJECT(m_pipeline->element()),
-            "suburi", NULL);
+        g_object_set(G_OBJECT(m_pipeline->element()),
+          "suburi", NULL, NULL);
     } else {
         QString fontDesc;
         QByteArray customFont = qgetenv("PHONON_SUBTITLE_FONT");