430e039a9594d6971d83a5c95f75de07829cfd98
Luigi Toscano <luigi.toscano@tiscali.it>
2019-07-03 23:23:34 +0200

    Fix the message extraction the *5 way
    Phonon translations are tracked by Qt5 branches and it makes sense
    to adapt them to the Qt5 conventions.
    - port to EXTRACT_TR_STRINGS, because XGETTEXT_QT was only
      available for *4 i18n branches;
    - add the _qt suffix, which identifies libraries which use the
      Qt native translation system.

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,3 @@
 #! /usr/bin/env bash
-find gstreamer/ -maxdepth 1 -name "*.cpp" -print > files
-find gstreamer/ -maxdepth 1 -name "*.h" -print >> files
-$XGETTEXT_QT --copyright-holder=This_file_is_part_of_KDE --msgid-bugs-address=https://bugs.kde.org --files-from=files -o $podir/phonon_gstreamer.pot
-rm files
+#! /usr/bin/env bash
+$EXTRACT_TR_STRINGS $(find . -name "*.cpp" -o -name "*.h") -o $podir/phonon_gstreamer_qt.pot
